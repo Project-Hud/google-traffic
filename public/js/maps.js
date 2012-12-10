b6908632d3f590fe72b772e514d27896709463b2
Tom Gallacher <tomgallacher23@gmail.com>
2012-12-10 14:32:13 +0000

    Cleaning up codebase

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,16 +1,30 @@
-var origin = new google.maps.LatLng(51.69356215411849, -0.43700695037841797)
-var mapOptions =
-  { zoom: 13
-  , center: origin
-  , mapTypeId: google.maps.MapTypeId.ROADMAP
-  , disableDefaultUI: true
+var initialize = function() {
+  var origin = new google.maps.LatLng(51.69356215411849, -0.43700695037841797)
+  var mapOptions =
+    { zoom: 13
+    , center: origin
+    , mapTypeId: google.maps.MapTypeId.ROADMAP
+    , disableDefaultUI: true
+    }
+
+  var map = new google.maps.Map(document.getElementById('traffic'), mapOptions)
+
+  var trafficLayer = new google.maps.TrafficLayer()
+  trafficLayer.setMap(map)
+
+  if (navigator.geolocation) {
+    // Get current position
+    navigator.geolocation.getCurrentPosition(function (position) {
+      // Success!
+        origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      }
+    )
   }
 
-var map = new google.maps.Map(document.getElementById('traffic'), mapOptions)
+  setInterval(function() {
+    map.panTo(origin);
+  }, 1000)
+}
 
-var trafficLayer = new google.maps.TrafficLayer()
-trafficLayer.setMap(map)
 
-setInterval(function() {
-  map.panTo(origin);
-}, 1000)
+google.maps.event.addDomListener(window, 'load', initialize);

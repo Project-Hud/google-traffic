eb140c648a510c595d409e54fb88f2487d3dbfde
Tom Gallacher <tomgallacher23@gmail.com>
2012-12-11 14:46:56 +0000

    Adding kinect control to maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,13 +1,16 @@
 var apiLocation = '/getData'
   , markers = []
 
+
 var initialize = function() {
   var origin = new google.maps.LatLng(51.69356215411849, -0.43700695037841797)
+    , pixelLatLng = origin
   var mapOptions =
-    { zoom: 10
+    { zoom: 11
     , center: origin
     , mapTypeId: google.maps.MapTypeId.ROADMAP
     , disableDefaultUI: true
+    , keyboardShortcuts: true
     }
 
   var map = new google.maps.Map(document.getElementById('traffic'), mapOptions)
@@ -24,9 +27,48 @@ var initialize = function() {
     )
   }
 
-  setInterval(function() {
-    map.panTo(origin)
-  }, 1000)
+  var overlay = new google.maps.OverlayView()
+  overlay.draw = function() {}
+  overlay.setMap(map)
+
+
+  DepthJS = {
+    onKinectInit: function() {
+      console.log('init');
+    },
+    onRegister: function(x, y, z, data) {
+      console.log('reg')
+      console.log(x,y,z,data)
+    },
+    onUnregister: function() {
+      console.log('bye')
+      map.panTo(origin)
+    },
+    onSwipeLeft: function() {
+    },
+    onMove: function(x,y, z) {
+      var resX = ( x - 1 ) / ( 100 - 1 ) * ($(document).width())
+      var resY = ( y - 1 ) / ( 100 - 1 ) * ($(document).height())
+      pixelLatLng = overlay.getProjection().fromDivPixelToLatLng(new google.maps.Point(resX,resY))
+      map.panTo(pixelLatLng)
+    },
+    onSwipeRight: function() {
+      console.log('right')
+    },
+    onSwipeDown: function() {
+      console.log('down')
+    },
+    onSwipeUp: function() {
+      console.log('up')
+    },
+    onPush: function() {
+      console.log('push')
+    },
+    onPull: function() {
+      console.log('pull!!!')
+    },
+    verbose: false
+  };
 
   $.getJSON(apiLocation, function(data) {
     var jams = data.jams
